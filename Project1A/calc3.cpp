c193c4ad5277e990dc162bc49c4b9e9bf100dec8
TalhaR <delta.talha@gmail.com>
2019-02-02 18:32:14 -0500

    finished final part of project

---
+++ Project1A/calc3.cpp
@@ -0,0 +1,54 @@
+/*
+Author: Talha Rahman
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: Project 1A - Task D
+*/
+
+// This program is the improved version of Calc 2. 
+// It reads from a text file that should 
+// contain numbers that are either added or subtracted
+// and prints out the total of multiple equations
+// In addition it can square numbers
+// requires linux command "./calc3 < formulas.txt"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char input; // initial input
+    int num; // integer version of the char(s) input
+    int total = 0;
+    int nextNum = 0; 
+    bool add = true;
+    
+    while (cin >> input) {                                                                                                             
+        if (input == '+') { 
+            add ? (total += nextNum) : (total -= nextNum);
+            add = true;
+            nextNum = 0;      
+        } else if (input == '-') {
+            add ? (total += nextNum) : (total -= nextNum);
+            add = false;
+            nextNum = 0;      
+        } else if (input == '^') {
+            add ? (total += (nextNum * nextNum)) : (total -= (nextNum * nextNum));
+            nextNum = 0;
+        } else if (input == ';'){ // prints total then resets all the values to 0
+            add ? (total += nextNum) : (total -= nextNum);
+            cout << total << "\n";
+            total = 0; 
+            nextNum = 0;
+            add = true;
+        } else {
+            nextNum *= 10; // need to multiply by 10 to shift number from ones to tens place, or tens to hundreds
+            num = input - 48; // on the ascii table numbers start at 48. So subtract by 48 to convert to digits
+            nextNum += num;      
+        } 
+    }
+    add ? (total += nextNum) : (total -= nextNum);
+    if (total != 0){
+        cout << total << "\n";
+    }
+}
